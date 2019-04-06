b93ddbc39c1b4e81f611b4b56d822bc6b2313941
wiktor <wiktor.nizio@gmail.com>
2019-04-06 05:16:25 +0200

    Initial commit.

---
+++ mvvm/src/main/java/pl/org/seva/myapplication/main/MyApplication.kt
@@ -0,0 +1,17 @@
+package pl.org.seva.myapplication.main
+
+import android.app.Application
+import org.kodein.di.Kodein
+import org.kodein.di.conf.global
+
+@Suppress("unused")
+class MyApplication : Application() {
+    init {
+        Kodein.global.addImport(module)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        bootstrap.boot()
+    }
+}
