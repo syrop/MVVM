05fdffbcf9946882fbcf38c32b23480540911f02
wiktor <wiktor.nizio@gmail.com>
2019-04-06 16:09:19 +0200

    Removed redundant binding.

--- mvvm/src/main/java/pl/org/seva/mvvm/main/KodeinModuleBuilder.kt
+++ mvvm/src/main/java/pl/org/seva/mvvm/main/KodeinModuleBuilder.kt
@@ -24,30 +24,18 @@ import org.kodein.di.Kodein
 import org.kodein.di.conf.global
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
-import org.kodein.di.generic.multiton
 import org.kodein.di.generic.singleton
-import pl.org.seva.mvvm.BuildConfig
 import pl.org.seva.mvvm.model.ActivityRecognitionObservable
 import pl.org.seva.mvvm.model.SensorActivityRecognitionObservable
-import java.util.logging.Logger
 
 val Context.module get() = KodeinModuleBuilder(this).build()
 
 inline fun <reified T : Any> instance() = Kodein.global.instance<T>()
 
-inline fun <reified A, reified T : Any> instance(arg: A) = Kodein.global.instance<A, T>(arg = arg)
-
 class KodeinModuleBuilder(private val ctx: Context) {
 
     fun build() = Kodein.Module("main") {
         bind<Bootstrap>() with singleton { Bootstrap() }
-        bind<Logger>() with multiton { tag: String ->
-            Logger.getLogger(tag)!!.apply {
-                if (!BuildConfig.DEBUG) {
-                    setFilter { false }
-                }
-            }
-        }
         bind<ActivityRecognitionObservable>() with  singleton { SensorActivityRecognitionObservable(ctx) }
     }
 }
